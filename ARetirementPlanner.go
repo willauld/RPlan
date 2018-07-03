5c100cd56bdf02d08f0ef520b7c767da53834371
willauld <will.auld@gmail.com>
2018-07-02 17:53:16 -0700

    Adding the command line output to help message

--- ARetirementPlanner.go
+++ ARetirementPlanner.go
@@ -194,6 +194,11 @@ func listInputParamsStrMap(f *os.File) {
 }
 
 func help() {
+	fmt.Printf("\n'%s", filepath.Base(os.Args[0]))
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf(" %s", os.Args[i])
+	}
+	fmt.Printf("'\n\n")
 	fmt.Printf("\nUsage: %s [options]* configfile\n", filepath.Base(os.Args[0]))
 	pflag.PrintDefaults()
 	os.Exit(0)
